d486f2a26a2bd242e025953c18a4f32d65e4097f
ccocchi <cocchi.c@gmail.com>
2021-01-28 12:44:07 +0100

    First commit

---
+++ lib/rails_logging_formatters.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_logging_formatters/version"
+
+module RailsLoggingFormatters
+  class Error < StandardError; end
+  # Your code goes here...
+end
